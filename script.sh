1dc6f56d07b108fe9545060967049b0c7fc97be5
Rainy99 <y@devzhou.cn>
2021-02-22 15:08:16 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -25,3 +25,7 @@ mv -f package-temp/luci-app-vssr package/lean/
 rm -rf package-temp
 
 git clone https://github.com/godros/luci-app-godproxy.git package/luci-app-godproxy
+git clone https://github.com/garypang13/luci-app-bypass.git package/luci-app-bypass
+
+find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
+find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
